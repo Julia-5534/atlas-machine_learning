e58f7b607bd45124dd9071f5cedc3f6fa4bd09a1
Julia Bullard <5534@holbertonstudents.com>
2024-01-10 15:27:23 -0600

    More documentation added

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -5,17 +5,44 @@ import numpy as np
 
 
 def play(env, Q, max_steps=100):
-    """Plays Frozen Lake"""
+    """
+    Plays the Frozen Lake game using a trained model.
+
+    Parameters:
+    env: The Frozen Lake environment.
+    Q: The Q-table.
+    max_steps (int, optional): The maximum number of steps
+    per game. Default is 100.
+
+    Returns:
+    total_rewards: The total reward for the game.
+    """
+
+    # Reset the environment to the starting state
     state = env.reset()
     total_rewards = 0
 
     for step in range(max_steps):
+        # Choose the action with the highest expected reward
         action = np.argmax(Q[state, :])
-        new_state, reward, done, _ = (env.step(action)[:4])
+
+        # Take the chosen action and observe the new state and reward
+        new_state, reward, done, _ = env.step(action)[:4]
+
+        # Update the total reward
         total_rewards += reward
+
+        # Render the environment
         env.render()
+
+        # Update the state
         state = new_state
+
+        # Update the total reward
         total_rewards += reward
+
+        # End the game if the done flag is True
         if done:
             break
+
     return total_rewards
