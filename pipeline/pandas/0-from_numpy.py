419b7f367a46e756a93c7d4147a6d4201a764acc
Julia Bullard <5534@holbertonstudents.com>
2024-02-12 17:03:43 -0600

    Task 0

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Task 0"""
+
+import pandas as pd
+import string
+
+
+def from_numpy(array):
+    """Ceates a pd.DataFrame from a np.ndarray"""
+    # Create a list of uppercase letters in the alphabet
+    labels = list(string.ascii_uppercase)
+    
+    # Create a DataFrame from the ndarray
+    df = pd.DataFrame(array)
+    
+    # Assign the column labels
+    df.columns = labels[:len(df.columns)]
+    
+    return df
