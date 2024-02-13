85ef697b50d34338e9ec3fad681b8dd0ab8d8eef
Julia Bullard <5534@holbertonstudents.com>
2024-02-13 00:06:03 -0600

    Need to see Checker Errors

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -6,6 +6,7 @@ import datetime
 
 
 def get_first_launch():
+    """Displays the First Launch"""
     response = requests.get('https://api.spacexdata.com/v4/launches')
     launches = response.json()
 
@@ -25,18 +26,26 @@ def get_first_launch():
     date_local = datetime.datetime.fromtimestamp(date_unix)
 
     # Get the rocket name
-    rocket_response = requests.get(f'https://api.spacexdata.com/v4/rockets/{rocket_id}')
+    rocket_response = requests.get(
+        'https://api.spacexdata.com/v4/rockets/{}'.format(rocket_id))
     rocket_name = rocket_response.json()['name']
 
     # Get the launchpad name and locality
-    launchpad_response = requests.get(f'https://api.spacexdata.com/v4/launchpads/{launchpad_id}')
+    launchpad_response = requests.get(
+        'https://api.spacexdata.com/v4/launchpads/{}'.format(launchpad_id))
     launchpad_name = launchpad_response.json()['name']
     launchpad_locality = launchpad_response.json()['locality']
 
     # Format the output
-    output = f"{launch_name} ({date_local}) {rocket_name} - {launchpad_name} ({launchpad_locality})"
-    
+    output = "{} ({}) {} - {} ({})".format(
+        launch_name,
+        date_local,
+        rocket_name,
+        launchpad_name,
+        launchpad_locality)
+
     return output
+
 
 if __name__ == '__main__':
     print(get_first_launch())
