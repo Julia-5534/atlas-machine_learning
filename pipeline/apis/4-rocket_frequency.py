8b9ebdbcd9bdd4446d85d2f8f76210d59855b41d
Julia Bullard <5534@holbertonstudents.com>
2024-02-13 01:01:22 -0600

    Try this for Task 4

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -2,33 +2,38 @@
 """Task 4"""
 
 import requests
-from collections import defaultdict
+from collections import Counter
+
+launch_url = 'https://api.spacexdata.com/v3/launches'
+rocket_url = 'https://api.spacexdata.com/v3/rockets'
 
 
-def get_launches():
-    """Gets The Launches"""
-    response = requests.get('https://api.spacexdata.com/v3/launches')
-    data = response.json()
-    return data
+def get_launches_per_rocket():
+    """Displays the number of launches per rocket"""
+    # Get the launch and rocket data
+    launches = requests.get(launch_url).json()
+    rockets = requests.get(rocket_url).json()
 
+    # Create a dictionary to store the rocket names
+    rocket_names = {
+        rocket['rocket_id']: rocket['rocket_name'] for rocket in rockets}
 
-def count_launches(launches):
-    """Counts The Number Of Launches"""
-    count_dict = defaultdict(int)
-    for launch in launches:
-        rocket_name = launch['rocket']['rocket_name']
-        count_dict[rocket_name] += 1
-    return count_dict
+    # Get the rocket id for each launch and count the number of launches
+    launched_rockets = [
+        launch['rocket']['rocket_id'] for launch in launches]
+    launches_per_rocket = Counter(launched_rockets)
 
+    # Replace the rocket ids with the rocket names and sort the result
+    rockets_and_launches = {
+        rocket_names[
+            rocket_id]: count for rocket_id,
+        count in launches_per_rocket.items()}
+    sorted_rockets_and_launches = sorted(
+        rockets_and_launches.items(), key=lambda x: (-x[1], x[0]))
 
-def print_sorted_counts(count_dict):
-    """Prints Sorted Counts"""
-    sorted_counts = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
-    for rocket, count in sorted_counts:
-        print("{}: {}".format(rocket, count))
+    return sorted_rockets_and_launches
 
 
 if __name__ == '__main__':
-    launches = get_launches()
-    count_dict = count_launches(launches)
-    print_sorted_counts(count_dict)
+    for rocket_name, num_launches in get_launches_per_rocket():
+        print("{}: {}".format(rocket_name, num_launches))
