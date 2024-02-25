3a585717ff236b8fbb0375d05598300317f17b0d
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 16:39:57 -0600

    Added name to

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Task 32"""
 
+
 def update_topics(mongo_collection, name, topics):
     """Updates all topics of a school document based on the name"""
-    result = mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    result = mongo_collection.update_many(
+        {"name": name}, {"$set": {"name": name, "topics": topics}})
     return result
