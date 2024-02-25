7d32c130f94f6d19579dd45fd092b36152a92057
Julia-5534 <112386137+Julia-5534@users.noreply.github.com>
2024-02-25 13:38:01 -0600

    Update 0-create_database_if_missing.sql

--- pipeline/databases/0-create_database_if_missing.sql
+++ pipeline/databases/0-create_database_if_missing.sql
@@ -1,6 +1,2 @@
-#!/usr/bin/env python3
-"""Task 0"""
-
-
-# Creates the db db_0 in MySQL server
-CREATE DATABASE IF NOT EXISTS db_0;
+-- Creates the db db_0 in MySQL server
+CREATE DATABASE IF NOT EXISTS 'db_0';
