1f307797a7ad64386a77f553380e851699e7f645
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 16:37:01 -0600

    Removed .modifiedcount

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -4,4 +4,4 @@
 def update_topics(mongo_collection, name, topics):
     """Updates all topics of a school document based on the name"""
     result = mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
-    return result.modified_count
+    return result
