d281184e0c4cef27084d703023d239c22fb36e1d
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 16:04:26 -0600

    Task 21 Updated

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -4,10 +4,11 @@ DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
     DECLARE result FLOAT;
+
     IF b = 0 THEN
-        RETURN 0;
+        SET result = 0;
     ELSE
-        RETURN a / b;
+        SET result = a / b;
     END IF;
 
     RETURN result
