2edaa093e577da1bd5861d963a54e39261df56d8
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 15:48:34 -0600

    Updated IFNULL and changed main_style to style

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+SELECT band_name, IFNULL((split - formed), (2020 - formed)) AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
