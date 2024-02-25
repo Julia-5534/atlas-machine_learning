1785169456d242d5419405ff989753df14fd50b4
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 15:05:58 -0600

    Wrong names and INNER join needed

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT genre, COUNT(show_id) AS number_of_shows
-FROM tv_show_genres
-JOIN genres ON tv_show_genres.genre_id = genres.id
-GROUP BY genre
-HAVING COUNT(show_id) > 0
-ORDER BY number_of_shows DESC;
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC
