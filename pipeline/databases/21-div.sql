e3e6242c958939f5b11c0bcc1beb9f4565ce1ff9
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 16:02:42 -0600

    Task 21 Updated

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,11 +1,15 @@
 -- Creates a function SafeDiv
 DELIMITER //
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS INT
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
+    DECLARE result FLOAT;
     IF b = 0 THEN
         RETURN 0;
     ELSE
         RETURN a / b;
     END IF;
+
+    RETURN result
 END;//
 DELIMITER ;
