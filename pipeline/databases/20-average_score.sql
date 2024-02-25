900e474a7546a8e2a07b58f614ea062147586788
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 15:59:39 -0600

    Task 20 Updated

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,11 +1,12 @@
 -- Creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
 DELIMITER //
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
+    DECLARE avg_score DECIMAL(3, 1);
 
-    SELECT AVG(score) INTO avg_score FROM scores WHERE user_id = user_id;
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id=user_id;
 
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
+    UPDATE users SET users.average_score = avg_score WHERE users.id=user_id;
 END;//
 DELIMITER ;
