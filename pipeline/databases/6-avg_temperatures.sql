7494b6eb528b4e72db4f954f17d5f36f4f054248
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 14:59:46 -0600

    Forgot to update placeholders

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,5 +1,4 @@
 -- Displays avg temperature (F) by city ordered by temperature (descending)
-SELECT city, AVG(temperature) AS average_temperature
-FROM your_table_name
+SELECT city, AVG(value) AS avg_temp FROM temperatures
 GROUP BY city
-ORDER BY average_temperature DESC;
+ORDER BY avg_temp DESC;
--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,5 +1,4 @@
 -- Displays the max temperature of each state (ordered by State name)
-SELECT state, MAX(temperature) AS max_temperature
-FROM your_table_name
+SELECT state, MAX(value) AS max_temp FROM temperatures
 GROUP BY state
-ORDER BY state;
+ORDER BY STATE ASC;
