b94b400de7383f7a18ca0650f4b74e2f8676a75b
Julia Bullard <5534@holbertonstudents.com>
2024-02-25 16:34:12 -0600

    Task 31

---
+++ pipeline/databases/31-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Task 31"""
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection based on kwargs"""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
